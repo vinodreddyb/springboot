85713f79a1b03412b92055f649fa9b8abb478dee
vinod <vinodbatthula@gmail.com>
2015-07-26 22:13:26 +0530

    Facelets added

--- dbcreation/src/main/java/org/vinod/Main.java
+++ dbcreation/src/main/java/org/vinod/Main.java
@@ -1,18 +1,9 @@
 package org.vinod;
 
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.embedded.ServletListenerRegistrationBean;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.context.ServletContextAware;
-
-import com.sun.faces.config.ConfigureListener;
 
 @SpringBootApplication
 
